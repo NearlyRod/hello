9cee2f520face41d159b7095f5c7ee6511df45f2
NearlyRod <44687721+NearlyRod@users.noreply.github.com>
2022-01-03 15:09:02 -0600

    Update hello-openshift-for-developers.go

--- hello-openshift-for-developers.go
+++ hello-openshift-for-developers.go
@@ -9,7 +9,7 @@ import (
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	response := os.Getenv("RESPONSE")
 	if len(response) == 0 {
-		response = "Hello World... things are going Grr-eaa-t!!"
+		response = "Hello World... things are good"
 	}
 
 	fmt.Fprintln(w, response)
